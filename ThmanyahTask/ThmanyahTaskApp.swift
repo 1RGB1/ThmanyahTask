42dd037c0c60384943d5471492afaff51af8f1ec
1RGB1 <a.ragab2012@gmail.com>
2026-03-11 20:10:14 +0300

    Project start

---
+++ ThmanyahTask/ThmanyahTaskApp.swift
@@ -0,0 +1,18 @@
+// 
+//  ThmanyahTaskApp.swift
+//  ThmanyahTask
+//
+//  Created by Ahmad Ragab on 11/03/2026.
+//  Copyright © 2026 Ahmad Ragab. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct ThmanyahTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThmanyahTaskTests/ThmanyahTaskTests.swift
@@ -0,0 +1,18 @@
+// 
+//  ThmanyahTaskTests.swift
+//  ThmanyahTask
+//
+//  Created by Ahmad Ragab on 11/03/2026.
+//  Copyright © 2026 Ahmad Ragab. All rights reserved.
+//
+
+import Testing
+@testable import ThmanyahTask
+
+struct ThmanyahTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
